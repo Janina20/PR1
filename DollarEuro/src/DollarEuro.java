42d5f301d3d9a866732cca46481f02692a901837
student <student@192.168.101.20>
2018-10-16 13:40:02 +0200

    aufgabe1 fertig nur noch aufragbe2

--- DollarEuro/src/DollarEuro.java
+++ DollarEuro/src/DollarEuro.java
@@ -6,27 +6,30 @@ public class DollarEuro {
 		
 		
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Bitte geben Sie Euro an");
+		double euro;
+		double dollar;
 		
-		while(true){
-			String s = sc.next();
-			if (s.equals("1")) {
-				double euro = sc.nextFloat();
-				
-				double dollar = euro*1.34;
+		
+		do {
+			System.out.println("Gib einen Menuepunkt ein:");
+			String menu = sc.next();
+			if (menu.equals("1")) {
+				System.out.println("Bitte geben Sie Euro an");
+				euro = sc.nextFloat();
+				dollar = euro*1.34;
 				
 				System.out.println("Die Währung in Dollar beträgt:"+dollar);
 				
 				
 					
 			}
-			else if (s.equals("2")) {
+			else if(menu.equals("2")) {
 				System.out.println("Bitte geben Sie Dollar an");
 				dollar = sc.nextFloat();
-				
 				euro = dollar*0.66;
 				
 				System.out.println("Die Währung in Euro beträgt:"+euro);	
 			}
-		}
+		}while();
+}
 }
