c5ef23573623aee4593597ca97c5c5c25ba370c5
janinaferencik <janinaferencik@janinas-mbp.fritz.box>
2018-10-18 14:12:20 +0200

    ready
    Signed-off-by: janinaferencik <janinaferencik@janinas-mbp.fritz.box>

--- DollarEuro/src/DollarEuro.java
+++ DollarEuro/src/DollarEuro.java
@@ -1,35 +1,42 @@
 import java.util.*;
+
 public class DollarEuro {
 
 	public static void main(String[] args) {
-		// Waehrungsrechner
-		
-		
-		Scanner sc = new Scanner (System.in);
+		// currency converter
+		// read from console
+		Scanner sc = new Scanner(System.in);
+		// data type for creating variable
 		double euro;
 		double dollar;
-		
-		
-		do {
-			System.out.println("Gib einen Menuepunkt ein:");
-			String menu = sc.next();
+		// create object menu of class
+		String menu;
+		// execute instructions
+		do { // output text on the console
+			System.out.println("Waehle 1 fuer Euro und 2 fuer Dollar:");
+			// read menu items
+			menu = sc.next();
+
 			if (menu.equals("1")) {
-				System.out.println("Bitte geben Sie Euro an");
-				euro = sc.nextFloat();
-				dollar = euro*1.34;
-				
-				System.out.println("Die Währung in Dollar beträgt:"+dollar);
-				
-				
-					
+				System.out.println("Bitte geben Sie Euro an:");
+				euro = sc.nextDouble();
+				dollar = euro * 1.34;
+
+				System.out.println("Die Waehrung in Dollar betraegt:" + dollar);
 			}
-			else if(menu.equals("2")) {
-				System.out.println("Bitte geben Sie Dollar an");
-				dollar = sc.nextFloat();
-				euro = dollar*0.66;
-				
-				System.out.println("Die Währung in Euro beträgt:"+euro);	
+
+			else if (menu.equals("2")) {
+				System.out.println("Bitte geben Sie Dollar an:");
+				dollar = sc.nextDouble();
+				euro = dollar * 0.66;
+
+				System.out.println("Die Waehrung in Euro betraegt:" + euro);
 			}
-		}while();
-}
+
+			System.out.println("Wollen Sie noch eine Umrechnung starten? Ja/Nein");
+			// execution of the statement as long as true
+		} while (sc.next().equals("Ja"));
+
+	}
+
 }
