649929eae3d54b27fe8cdca1ce68e3921a1f0c26
janinaferencik <janinaferencik@janinas-mbp.fritz.box>
2018-10-22 20:17:56 +0200

    ready4

--- DollarEuro/src/DollarEuro.java
+++ DollarEuro/src/DollarEuro.java
@@ -3,53 +3,47 @@ import java.util.*;
 public class DollarEuro {
 
 	public static void main(String[] args) {
-		// currency converter
-		// create a scanner to read from console
+
+		// create a scanner (and name it "sc") to read from console
 		Scanner sc = new Scanner(System.in);
-		// data type for creating variable
+		// declare variable
 		double euro;
 		double dollar;
-		// create object "menu" of class
-		String menu;
-		// execute instructions
-		
-		do { // output text on console
+		// start do-while-loop
+		do {
+			// print out text between "" on console
 			System.out.println("Waehle 1 fuer Euro und 2 fuer Dollar:");
-			// read menu items
-			menu = sc.next();
-			
-			// menu item 1 corresponds to input 1
-			if (menu.equals("1")) {
-				// output text on console
+			// open scanner for menu case
+			String wahl = sc.next();
+			// if condition for case 1 (Euro in Dollar)
+			if (wahl.equals("1")) {
+				// print out text on console
 				System.out.println("Bitte geben Sie Euro an:");
-				// declare euro as floating point number
+				// take variable euro for input
 				euro = sc.nextDouble();
 				// conversion
 				dollar = euro * 1.34;
-				// output text on console
+				// print out text + result on console
 				System.out.println("Die Waehrung in Dollar betraegt:" + dollar);
 			}
-			
-			// menu item 2 corresponds to input 2
-			else if (menu.equals("2")) {
-				// output text on console
+			// if condition case 2 (Dollar in Euro)
+			else if (wahl.equals("2")) {
+				// print out text on console
 				System.out.println("Bitte geben Sie Dollar an:");
-				// declare dollar as floating point number
+				// take variable dollar for input
 				dollar = sc.nextDouble();
 				// conversion
 				euro = dollar * 0.66;
-				// output text on console
+				// print out text on console
 				System.out.println("Die Waehrung in Euro betraegt:" + euro);
 			}
-			
-			// output text on console
+			// print out text on console
 			System.out.println("Wollen Sie noch eine Umrechnung starten? Ja/Nein");
-			// execution of the statement as long as input "Ja"
+			// if input "Ja" got to do-loop
 		} while (sc.next().equals("Ja"));
 
-		// close the scanner input "Nein"
+		// if input "Nein" finish program and close Scanner
 		System.out.println("Umrechnung beendet...");
 		sc.close();
-
 	}
 }
